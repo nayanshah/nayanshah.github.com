7275ded31be55cea98689ea71445ed3f6e53e14e
Nayan Shah <nayan@nayanshah.com>
2015-10-17 10:13:17 -0700

    Reordering the classes in _tags.rb

--- _plugins/_tags.rb
+++ _plugins/_tags.rb
@@ -1,18 +1,4 @@
 module Jekyll
-  class TagPage < Page
-    def initialize(site, base, dir, tag)
-      @site = site
-      @base = base
-      @dir = dir
-      @name = "#{tag}.html"
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
-      tag_title_prefix = site.config['tag_title_prefix'] || 'Posts Tagged &ldquo;'
-      tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;'
-      self.data['tag'] = tag
-      self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
-    end
-  end
   class TagGenerator < Generator
     safe true
     def generate(site)
@@ -28,6 +14,20 @@ module Jekyll
       index.render(site.layouts, site.site_payload)
       index.write(site.dest)
       site.pages << index
+    end
+  end
+  class TagPage < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = "#{tag}.html"
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      tag_title_prefix = site.config['tag_title_prefix'] || 'Posts Tagged &ldquo;'
+      tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;'
+      self.data['tag'] = tag
+      self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
     end
   end
 end
